4d89ea606d6af38620cf4a5c77a2567bd9aa84c0
Piotr Trojan <piotr.trojan@euvic.pl>
2021-05-29 18:18:57 +0200

    Solution created. Simple, emtpy WebApi project added with empty controller

---
+++ source/AwesomeBankingApp/AwesomeBankingApp.Api/Controllers/LoanController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AwesomeBankingApp.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoanController : ControllerBase
+    {
+        public LoanController()
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
